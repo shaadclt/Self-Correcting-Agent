c7f27a6ecaaa6c06845f61e1f0845b33f0405b45
Mohamed Shaad <shaadclt@gmail.com>
2026-05-22 07:28:03 +0530

    create executor.py

---
+++ executor.py
@@ -0,0 +1,27 @@
+import subprocess
+
+def run_code(file_path):
+    try:
+        result = subprocess.run(
+            ["python", file_path],
+            capture_output=True,
+            text=True,
+            timeout=10
+        )
+
+        if result.returncode == 0:
+            return {
+                "success": True,
+                "output": result.stdout
+            }
+
+        return {
+            "success": False,
+            "error": result.stderr
+        }
+
+    except Exception as e:
+        return {
+            "success": False,
+            "error": str(e)
+        }
